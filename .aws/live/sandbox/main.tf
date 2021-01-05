e1e28a4702a459e2e958a8cb8986416c7b1436f0
Su <tracysu1990@hotmail.com>
2021-01-05 16:36:42 -0500

    update

--- .aws/live/sandbox/main.tf
+++ .aws/live/sandbox/main.tf
@@ -3,9 +3,9 @@ provider "aws" {
   # access_key = var.AWS_ACCESS_KEY_ID
   # secret_key = var.AWS_SECRET_ACCESS_KEY
   
-  # assume_role {
-  #   role_arn = "arn:aws:iam::838374399476:role/admin"
-  # }  
+  assume_role {
+    role_arn = "arn:aws:iam::838374399476:role/admin"
+  }  
 }
 
 terraform {
